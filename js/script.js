e2896c047816c40c7666b4d877a4e00b2510afd3
fox1333 <55022909+fox1333@users.noreply.github.com>
2020-12-17 23:06:56 +0200

    a

--- js/script.js
+++ js/script.js
@@ -16,3 +16,13 @@ $(document).ready(function () {
 		});
 	}
 });
+function ibg() {
+
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+
+ibg();
